544d4f89abac1025ed744a6c265db38ad2152ea2
Adrian Lopez <ja044840@gmail.com>
2023-05-05 19:31:34 +0200

    NavBars little finish, adios fernando

--- dist/client/processes/js/buscador.js
+++ dist/client/processes/js/buscador.js
@@ -6,7 +6,6 @@ inputBusqueda.addEventListener("input", filtrarProductos);
 function filtrarProductos() {
     var textoBusqueda = inputBusqueda.value.toLowerCase();
     var productos = document.getElementsByClassName("p-3"); // Cambié la clase de "producto" a "p-3"
-    
     for (var i = 0; i < productos.length; i++) {
         var producto = productos[i];
         var titulo = producto.getElementsByTagName("p")[0].textContent.toLowerCase(); // Cambié la etiqueta de "h3" a "p"
@@ -14,6 +13,7 @@ function filtrarProductos() {
             producto.style.display = "block";
         } else {
             producto.style.display = "none";
+
         }
     }
 }
