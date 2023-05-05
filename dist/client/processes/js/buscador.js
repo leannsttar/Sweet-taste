2b5ede3617e498fb88467def32b963441803ef97
Rodri Pineda <105886719+Rodrogas666@users.noreply.github.com>
2023-05-05 12:41:07 -0300

    buscador funcionando, nacely x leandro
    sí

--- dist/client/processes/js/buscador.js
+++ dist/client/processes/js/buscador.js
@@ -1,22 +1,22 @@
 
-    var inputBusqueda = document.getElementById("search");
+var inputBusqueda = document.getElementById("search");
+
+inputBusqueda.addEventListener("input", filtrarProductos);
+
+function filtrarProductos() {
+    var textoBusqueda = inputBusqueda.value.toLowerCase();
+    var productos = document.getElementsByClassName("p-3"); // Cambié la clase de "producto" a "p-3"
     
-    inputBusqueda.addEventListener("input", filtrarProductos);
-    
-    function filtrarProductos() {
-        var textoBusqueda = inputBusqueda.value.toLowerCase(); 
-        var productos = document.getElementsByClassName("producto");
-        
-        
-        for (var i = 0; i < productos.length; i++) {
-            var producto = productos[i];
-            var titulo = producto.getElementsByTagName("h3")[0].textContent.toLowerCase(); 
-            if (titulo.includes(textoBusqueda)) {
-                producto.style.display = "block"; 
-            } else {
-                producto.style.display = "none"; 
-            }
+    for (var i = 0; i < productos.length; i++) {
+        var producto = productos[i];
+        var titulo = producto.getElementsByTagName("p")[0].textContent.toLowerCase(); // Cambié la etiqueta de "h3" a "p"
+        if (titulo.includes(textoBusqueda)) {
+            producto.style.display = "block";
+        } else {
+            producto.style.display = "none";
         }
     }
+}
+
 
 //No lo toquen xd, que hay que adaptarlo aún, pero sí funca
