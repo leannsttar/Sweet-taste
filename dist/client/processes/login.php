e7e874534e13c20c896e14cb8fa9b5d4dc789520
lpzxm <88464727+lpzxm@users.noreply.github.com>
2023-05-08 01:58:08 -0600

    hola leandro, me gustas mucho

--- dist/client/processes/login.php
+++ dist/client/processes/login.php
@@ -6,18 +6,28 @@ $conexionBD = BD::crearInstancia();
 if ($_POST) {
     $correo = $_POST['correo'];
     $password = $_POST['password'];
-
     $sql = $conexionBD->prepare('SELECT * FROM users WHERE correo=:correo AND password=:password');
     $sql->bindParam(':correo', $correo);
     $sql->bindParam(':password', $password);
     $sql->execute();
     $usuario = $sql->fetch(PDO::FETCH_ASSOC);
 
+    //Admi
+    $sqlAdmi = $conexionBD->prepare('SELECT * FROM admin WHERE correo=:correo AND password=:password');
+    $sqlAdmi->bindParam(':correo', $correo);
+    $sqlAdmi->bindParam(':password', $password);
+    $sqlAdmi->execute();
+    $Admi = $sqlAdmi->fetch(PDO::FETCH_ASSOC);
+
     if ($usuario) {
         $_SESSION['usuario'] = $usuario['id'];
         header('Location:  ../views/index.php');
         exit();
-    } else {
+    } else if ($Admi){
+        $_SESSION['admi'] = $admin['id'];
+        header ('Location: ../../admin/views/productos_admin.php');
+    }
+    else {
         echo "<script>
         alert('Contraseña o usuario incorrecto :)');
         window.location.href = '../views/form-login.php';
